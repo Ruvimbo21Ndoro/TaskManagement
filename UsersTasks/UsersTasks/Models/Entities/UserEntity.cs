f5fb6ac4f5c810a5901365d292cba4acfcef175a
ruvimbo ndoro <220128871@mycput.ac.za>
2025-08-24 22:11:14 +0200

    Add data annotation to the user entity for data validation

--- UsersTasks/UsersTasks/Models/Entities/UserEntity.cs
+++ UsersTasks/UsersTasks/Models/Entities/UserEntity.cs
@@ -5,13 +5,17 @@ namespace UsersTasks.Models.Entities
     public class UserEntity : BaseEntity
     {
 
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, ErrorMessage = "Username must not exceed 50 characters")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
         public string Password { get; set; }
+
     }
 }

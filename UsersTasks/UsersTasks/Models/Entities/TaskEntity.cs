2396938dc9f77b3b24a5ea24a37eac06498c502c
ruvimbo ndoro <220128871@mycput.ac.za>
2025-08-24 21:51:47 +0200

    Add data annotation to the task entity for data validation

--- UsersTasks/UsersTasks/Models/Entities/TaskEntity.cs
+++ UsersTasks/UsersTasks/Models/Entities/TaskEntity.cs
@@ -4,16 +4,18 @@ namespace UsersTasks.Models.Entities
 {
     public class TaskEntity : BaseEntity
     {
-        [Required]
-        public string  Title { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title must not exceed 100 characters")]
+        public string Title { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description must not exceed 500 characters")]
         public string Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Due date is required")]
         public DateOnly DueDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Assignee is required")]
         public Guid Assignee { get; set; }
         public UserEntity? User { get; set; } // Navigation property to the UserEntity
 
